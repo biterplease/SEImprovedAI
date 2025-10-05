9a821ca1a9f45c941f77ddea9f832bc14bfcfedf
biterplease <biterplease43@gmail.com>
2025-10-05 21:13:05 +0200

    remove unused method from thrustdata

--- ImprovedAI/Data/Scripts/ImprovedAI/ThrustData.cs
+++ ImprovedAI/Data/Scripts/ImprovedAI/ThrustData.cs
@@ -18,28 +18,5 @@ namespace ImprovedAI
         public float MaxForwardBackward => Math.Max(Forward, Backward);
         public float MaxUpDown => Math.Max(Up, Down);
         public float MaxLeftRight => Math.Max(Left, Right);
-
-        public void CalculateThrust(List<IMyThrust> thrusters)
-        {
-            foreach (var thruster in thrusters)
-            {
-                if (!thruster.IsWorking) continue;
-
-                Vector3I thrustDirection = thruster.GridThrustDirection;
-
-                if (thrustDirection == Base6Directions.GetIntVector(Base6Directions.Direction.Forward))
-                    Forward += thruster.MaxEffectiveThrust;
-                else if (thrustDirection == Base6Directions.GetIntVector(Base6Directions.Direction.Backward))
-                    Backward += thruster.MaxEffectiveThrust;
-                else if (thrustDirection == Base6Directions.GetIntVector(Base6Directions.Direction.Up))
-                    Up += thruster.MaxEffectiveThrust;
-                else if (thrustDirection == Base6Directions.GetIntVector(Base6Directions.Direction.Down))
-                    Down += thruster.MaxEffectiveThrust;
-                else if (thrustDirection == Base6Directions.GetIntVector(Base6Directions.Direction.Left))
-                    Left += thruster.MaxEffectiveThrust;
-                else if (thrustDirection == Base6Directions.GetIntVector(Base6Directions.Direction.Right))
-                    Right += thruster.MaxEffectiveThrust;
-            }
-        }
     }
 }
